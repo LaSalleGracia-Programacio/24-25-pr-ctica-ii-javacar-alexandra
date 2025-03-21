58ce32d54d277772a7250a565270f7ab1c326d85
Andreu <andreu.riba@gracia.lasalle.cat>
2025-03-21 13:06:45 +0100

    Canvis

---
+++ src/main/java/org/JavaCar/Roda.java
@@ -0,0 +1,12 @@
+package org.JavaCar;
+
+public class Roda {
+    private String marca;
+    private int diametre;
+
+
+    public Roda(String marca, int diametre) {
+        this.marca = marca;
+        this.diametre = diametre;
+    }gi
+}
